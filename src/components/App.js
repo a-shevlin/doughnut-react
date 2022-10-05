01148a9b725bfb5a7d45355090c638cdb31df08d
Alex Shevlin <alexshevlin1@gmail.com>
2022-10-05 13:00:36 -0700

    adjust file layout and import dependencies.

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <React.Fragment>
+      {/* <div className="container"> */}
+        <Header />
+      {/* </div> */}
+    </React.Fragment>
+  );
+}
+
+export default App;
